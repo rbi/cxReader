f3a73d6f54c207dc868d6058b4cee4437a5ef05a
Raik Bieniek <raik.bieniek@googlemail.com>
2011-12-21 20:54:29 +0100

    created restful web service stub

---
+++ connector-rest/src/main/java/org/dyndns/beefochu/cxreader/connector/rest/FeedReaderResource.java
@@ -0,0 +1,29 @@
+package org.dyndns.beefochu.cxreader.connector.rest;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.dyndns.beefochu.cxreader.backend.Reader;
+
+@Named
+@Path("/")
+public class FeedReaderResource {
+
+	@Inject
+	Reader reader;
+	
+    public FeedReaderResource() {
+    }
+
+    @GET
+    @Path("feeds/")
+    @Produces(MediaType.TEXT_PLAIN) //text/x-opml
+    public String getFeedList() {
+		return "Test";
+
+    }
+}
