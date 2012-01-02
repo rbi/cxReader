5d2967a3fde9509be5a03aa3b368327d54bf0070
Raik Bieniek <raik.bieniek@googlemail.com>
2012-01-02 19:38:52 +0100

    remove jersey specific settings

---
+++ connector-rest/src/main/java/org/dyndns/beefochu/cxreader/connector/rest/config/ApplicationConfig.java
@@ -0,0 +1,9 @@
+package org.dyndns.beefochu.cxreader.connector.rest.config;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class ApplicationConfig extends Application {
+
+}
